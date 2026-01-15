e08fb688358ed26bb4eb5fd19602ccbfb03ad48b
Victor López <victorlopez_20@outlook.com>
2026-01-15 16:58:59 -0600

    new: Creating my main routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,74 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+/* 
+    Ruta principal de la aplicación.
+
+    http://localhost:8000
+    http://localhost:8000?name=Andrés
+
+    $request -> Controla los datos que vienen en la petición
+
+    ?name=Name -> Parámetro opcional que se puede enviar en la URL
+*/
+Route::get('/', function (Request $request) {
+    $nombre = $request->name;
+
+    $message = "Bienvenido a Laravel!<br/>";
+
+    if ($nombre != NULL) {
+        $message .= "Hola, " . $nombre . "!";
+    }
+
+    // Retornamos la vista o información
+    return $message;
+});
+
+/* 
+    Ruta principal de la aplicación.
+
+    http://localhost:8000/info/{limit}
+
+    {limit} -> Parámetro obligatorio que se debe enviar en la URL
+*/
+Route::get('/info/{limit}', function(Request $request, $limit) {
+    // Generamos un número aleatorio
+    $num = rand(1, $limit);
+
+    if ($limit < 1) {
+        return "El límite debe ser un número mayor o igual a 1.";
+    }
+
+    $response = "Número aleatorio generado: " . $num . ". ";
+
+    return $response;
+});
+
+/* 
+    Ruta principal de la aplicación.
+
+    http://localhost:8000/about
+*/
+Route::get('/about', function() {
+    $services = array(
+        'Desarrollo web',
+        'Diseño gráfico',
+        'Marketing digital',
+        'Consultoría tecnológica',
+        'Soporte técnico'
+    );
+
+    $message = "Servicios ofrecidos:<br/>";
+
+    foreach ($services as $service) {
+        $message .= "- " . $service . "<br/>";
+    }
+
+    return $message;
+});
+
+Route::get('/services', function() {
+    return "Servicios :D";
 });
